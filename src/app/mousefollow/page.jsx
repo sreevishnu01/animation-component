3e76951e4af778897378d48ad60b46335350d278
vishnu_p <vishnu.p@richylife.ae>
2024-01-08 11:26:49 +0400

    nav added and make sepprete page for animations

---
+++ src/app/mousefollow/page.jsx
@@ -0,0 +1,12 @@
+import Follow from "@/components/Follow";
+import React from "react";
+
+function page() {
+  return (
+    <div className="bg-white">
+      <Follow />
+    </div>
+  );
+}
+
+export default page;
---
+++ src/lib/navData.js
@@ -0,0 +1,12 @@
+export const navData = {
+  data: [
+    {
+      titile: "home",
+      link: "/",
+    },
+    {
+      titile: "mousefollw",
+      link: "/mousefollow",
+    },
+  ],
+};
